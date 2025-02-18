4d5dc8b207600fba4a75a9a5191ccf654e2241f1
nilondes <oxygen93@ya.ru>
2025-02-18 21:13:22 +0300

    added logic for get wallet and post transaction

--- e_wallet/api/models.py
+++ e_wallet/api/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 class Wallet(models.Model):
     wallet_uuid = models.IntegerField(unique=True)
-    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    amount = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0.00)])
+
+
+class Transaction(models.Model):
+    choices = (
+        ("DEPOSIT", "DEPOSIT"),
+        ("WITHDRAW", "WITHDRAW")
+    )
+    wallet_uuid = models.ForeignKey(Wallet, to_field="wallet_uuid", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0.01)])
+    operation = models.CharField(choices=choices)
--- e_wallet/api/views.py
+++ e_wallet/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from decimal import Decimal
+from rest_framework import generics
+from api.serializers import WalletSerializer, TransactionSerializer
+from api.models import Wallet, Transaction
+from rest_framework.exceptions import NotFound, ValidationError
 
-# Create your views here.
+
+class WalletView(generics.ListAPIView):
+    serializer_class = WalletSerializer
+    queryset = Wallet.objects.all()
+
+    def filter_queryset(self, queryset):
+        wallet_uuid = self.request.parser_context['kwargs']['wallet_uuid']
+        return queryset.filter(wallet_uuid=wallet_uuid)
+
+
+class TransactionView(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+
+    def filter_queryset(self, queryset):
+        wallet_uuid = self.request.parser_context['kwargs']['wallet_uuid']
+        return queryset.filter(wallet_uuid=wallet_uuid)
+
+    def perform_create(self, serializer):
+        wallet_uuid = self.request.parser_context['kwargs']['wallet_uuid']
+        operation = self.request.data.get('operation')
+        amount = Decimal(self.request.data.get('amount'))
+
+        try:
+            wallet = Wallet.objects.get(wallet_uuid=wallet_uuid)
+        except Wallet.DoesNotExist:
+            raise NotFound(detail=f"There is no wallet with uuid {wallet_uuid}")
+        else:
+            if operation == "DEPOSIT":
+                wallet.amount += amount
+            else:
+                if wallet.amount < amount:
+                    raise ValidationError(detail="Insufficient funds in the wallet.")
+                wallet.amount -= amount
+            wallet.save()
+            return serializer.save(wallet_uuid=wallet)
