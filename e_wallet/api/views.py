9e47e66799e881508814a4bde18ed77fcd02d01e
nilondes <oxygen93@ya.ru>
2025-02-25 14:26:00 +0300

    refactored logic for perform_create.

--- e_wallet/api/views.py
+++ e_wallet/api/views.py
@@ -17,35 +17,32 @@ class WalletView(generics.RetrieveAPIView):
         cached_wallet = cache.get(cache_key)
         if cached_wallet:
             return cached_wallet
-        else:
-            try:
-                wallet = Wallet.objects.get(wallet_uuid=wallet_uuid)
-            except Wallet.DoesNotExist:
-                raise NotFound(detail=f"There is no wallet with uuid {wallet_uuid}")
-            cache.set(cache_key, wallet, timeout=60)
-            return wallet
-
-
-class TransactionView(generics.CreateAPIView):
-    serializer_class = TransactionSerializer
-
-    def perform_create(self, serializer):
-        wallet_uuid = self.kwargs['wallet_uuid']
-        operationType = self.request.data.get('operationType')
-        amount = Decimal(self.request.data.get('amount'))
         try:
             wallet = Wallet.objects.get(wallet_uuid=wallet_uuid)
         except Wallet.DoesNotExist:
             raise NotFound(detail=f"There is no wallet with uuid {wallet_uuid}")
+        cache.set(cache_key, wallet, timeout=60)
+        return wallet
+
+
+class TransactionView(generics.CreateAPIView):
+    serializer_class = TransactionSerializer
+    queryset = Wallet.objects.select_for_update()
+    lookup_field = 'wallet_uuid'
+
+    def perform_create(self, serializer):
+        wallet_uuid = self.kwargs['wallet_uuid']
+        operation_type = serializer.validated_data["operationType"]
+        amount = serializer.validated_data["amount"]
         with transaction.atomic():
-            wallet = Wallet.objects.select_for_update().get(wallet_uuid=wallet_uuid)
-            if operationType == "DEPOSIT":
+            wallet = self.get_object()
+            if operation_type == "DEPOSIT":
                 wallet.amount += amount
             else:
                 if wallet.amount < amount:
                     raise ValidationError(detail="Insufficient funds in the wallet.")
                 wallet.amount -= amount
             wallet.save()
+            serializer.save(wallet_uuid=wallet)
             cache_key = f'wallet_{wallet_uuid}'
             cache.set(cache_key, wallet, timeout=60)
-            return serializer.save(wallet_uuid=wallet)
